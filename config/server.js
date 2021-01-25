38442b33c4f2c310c7d9837bb494404b5e12b245
Alex Charland <ac3charland@gmail.com>
2021-01-25 09:03:23 -0600

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f1c858e4cd5633067a1fd7ed45e550b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e1869024-8957-4f02-8ea2-7f1796df1816'
+};
